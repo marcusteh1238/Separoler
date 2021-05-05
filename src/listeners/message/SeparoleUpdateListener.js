28559db88016e23059cd88c0649e5a0526f3eff4
marcusteh1238 <marcusteh1238@gmail.com>
2021-05-05 17:09:40 +0800

    ignore message on first guild join

--- src/listeners/message/SeparoleUpdateListener.js
+++ src/listeners/message/SeparoleUpdateListener.js
@@ -1,7 +1,13 @@
 const SeparoleHandler = require("../../SeparoleHandler")
 
+const { BOT_ID } = process.env;
+
 async function SeparoleUpdateListener(message) {
     if (!message.member) {
+        return null;
+    }
+    // ignore message on first guild join.
+    if (message.type === "GUILD_MEMBER_JOIN" && message.author.id === BOT_ID) {
         return null;
     }
     return SeparoleHandler(message.guild, message.member)
