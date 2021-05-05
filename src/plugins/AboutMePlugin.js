7a383ecdaa113ebf595aeff3c79b1a07fc2ba2f7
marcusteh1238 <marcusteh1238@gmail.com>
2021-05-05 13:11:18 +0800

    Add aboutme plugin

---
+++ src/plugins/AboutMePlugin.js
@@ -0,0 +1,53 @@
+const colors = require("../helpers/colors");
+const { PLUGIN_TYPES } = require("../helpers/constants");
+const getUserAvatarURL = require("../helpers/getUserAvatarURL");
+const Plugin = require("../structs/Plugin");
+
+const description = `I am a small utility bot that handles **__Separ__ator __Role__s**, known as **Separoles**, in your server.`
+const fields = [
+    {
+        name: "What are Separoles?",
+        value: "When you have tons of roles that users can equip, we understand that it can be a huge pain to look at. \n\nSetting up **[Separoles](https://www.reddit.com/r/discordapp/comments/a39fap/custom_role_separators/)** allows server owners to create many different types of roles (eg. levelled roles, color roles), without confusing new users when they look at your active members' profile."
+    },
+    {
+        name: "Why use Separoler Bot?",
+        value: "**Separoler Bot** (that's me!) helps you automatically manage **Separoles** using different configuration options, so your server moderators have one less thing to worry about when managing the server."
+    },
+    {
+        name: "Get Started Now!",
+        value: "Add me to your server with my invite link at `s!invite` or view a list of all my commands with `s!help`."
+    }
+]
+
+
+async function handle(message) {
+    return message.channel.send({
+        embed: {
+            title: "Hi, I am Separoler!",
+            description,
+            fields,
+            color: colors.info_lavendar,
+            footer: {
+                icon_url: getUserAvatarURL(message.author),
+                text: `Thanks for using Separoler, ${message.author.username}!`
+            }
+        }
+    })
+}
+
+const AboutMePlugin = new Plugin({
+    name: "aboutme",
+    aliases: ["about", "info"],
+    type: PLUGIN_TYPES[1],
+    help: {
+        description: "Let me tell you what I'm all about!",
+        usage: "s!aboutme",
+        examples: [
+            ["s!aboutme", "Views a summary about what Separoler does."]
+        ]
+    },
+    handle,
+    noDMs: false
+});
+
+module.exports = AboutMePlugin;
