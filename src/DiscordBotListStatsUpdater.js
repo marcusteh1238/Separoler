72f93be3833d7be2ceccfabf416898c42bbd68e8
marcusteh1238 <marcusteh1238@gmail.com>
2021-05-17 00:44:44 +0800

    add func to update dbl stats

---
+++ src/DiscordBotListStatsUpdater.js
@@ -0,0 +1,38 @@
+const axios = require("axios").default;
+
+const { getTotalGuildCount } = require("./DatabaseWrapper");
+const logger = require("./helpers/logger");
+
+const url = `https://discordbotlist.com/api/v1/bots/${process.env.BOT_ID}/stats`;
+const updateStatsCooldownMs = 10 * 60 * 1000; // 10 min
+
+let lastUpdated = 0;
+let lastCount = 0;
+
+const headers =  {
+            Authorization: process.env.DISCORD_BOT_LIST_TOKEN
+        }
+async function updateDBLStats() {
+    if (process.env.NODE_ENV !== "production") return;
+    if (Date.now() - lastUpdated < updateStatsCooldownMs) return;
+    lastUpdated = Date.now();
+    try {
+        const guildCount = parseInt(await getTotalGuildCount(), 10);
+        await axios.post(url, {
+            guilds: lastCount
+        }, { headers });
+        logger.info({
+            msg: "Updated number of guilds in Discord Bot List.",
+            oldCount: lastCount,
+            newCount: guildCount
+        });
+        lastCount = guildCount;
+    } catch (err) {
+        logger.error({
+            msg: "Error occurred while updating stats in discord bot list.",
+            err
+        });
+    }
+}
+
+module.exports = updateDBLStats;
